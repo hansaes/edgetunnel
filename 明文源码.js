be3a711d50ed74b8e1ee54c3a75db01393e17b7b
hansaes <147910031+hansaes@users.noreply.github.com>
2025-01-09 22:33:12 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = 'de9c99f2-b709-4766-a242-3311236a3ca5';
+let userID = '265e7023-913e-4061-9a09-7cc3e7371891';
 
 // 生成配置文件的 Cloudflare 优选 IP (www.gov.se   speed.cloudflare.com等使用Cloudflare CDN的网站也可以)
 const bestCFIP = "www.visa.com.hk"
